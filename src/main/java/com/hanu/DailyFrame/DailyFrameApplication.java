5d5309e712df19f785414bfbf620cc0bf4e14614
hanumantharaovaddi <hanumanthurao9381@gmail.com>
2025-12-21 11:27:55 +0530

    initial commit

---
+++ src/main/java/com/hanu/DailyFrame/DailyFrameApplication.java
@@ -0,0 +1,13 @@
+package com.hanu.DailyFrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyFrameApplication.class, args);
+	}
+
+}
